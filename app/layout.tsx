1f8e779fc4b3ab73fab73d2ee82c346cd6bcb23f
geekamita <amitasingh5661@gmail.com>
2023-12-12 22:35:11 +0530

    Basic Sidebar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,27 @@
-import type { Metadata } from 'next'
-import { Figtree } from 'next/font/google'
-import './globals.css'
+import type { Metadata } from "next";
+import { Figtree } from "next/font/google";
+import "./globals.css";
+import Sidebar from "@/components/Sidebar";
 
-const font = Figtree({ subsets: ['latin'] })
+const font = Figtree({ subsets: ["latin"] });
+
+console.log("Jai Shree Ram 🕉 🚩");
 
 export const metadata: Metadata = {
-  title: 'Spotify Clone',
-  description: 'Litsen to your favorite music',
-}
+  title: "Spotify Clone",
+  description: "Litsen to your favorite music",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body className={font.className}>{children}</body>
+      <body className={font.className}>
+        <Sidebar>{children}</Sidebar>
+      </body>
     </html>
-  )
+  );
 }
